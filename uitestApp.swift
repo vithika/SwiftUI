a4340c65da339ffefe04361278109c7cc8f604f2
vithika <vgvithikagupta1@gmail.com>
2022-07-05 16:06:11 +0530

    war card game

---
+++ uitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  uitestApp.swift
+//  uitest
+//
+//  Created by Vithika Gupta on 23/06/22.
+//
+
+import SwiftUI
+
+@main
+struct uitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
